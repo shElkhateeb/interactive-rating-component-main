dcf40dfea87a5c778aa32080125f04d2e959a41c
Shorouk Elkhateeb <shrouca@gmail.com>
2023-05-09 12:44:42 +0300

    Display thank you

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ const ratingScale = document.getElementById('ratingScale');
 const submitButton = document.getElementById('submit');
 let rate = 0;
 
-ratingScale.addEventListener('click', (event)=> {
+ratingScale.addEventListener('click', (event) => {
     if (event.target.tagName === 'INPUT') {
         rate = event.target.value;
         submitButton.disabled = false;
-    } 
+    }
+});
+
+let thankYou = document.getElementById('thankYouState');
+let rating = document.getElementById('ratingState');
+
+submitButton.addEventListener('click', () => {
+    ratingState.classList = 'invisible';
+    document.getElementById('rate').innerText = rate;
+    thankYou.classList = '';
+    submitButton.disabled = true;
 });
